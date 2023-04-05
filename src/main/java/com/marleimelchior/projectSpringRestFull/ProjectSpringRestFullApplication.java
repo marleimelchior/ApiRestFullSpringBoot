610bdb827d039f51f55b0d99fccb2e1d17e05429
Marlei Melchior <marleimelchior@gmail.com>
2023-04-05 10:19:35 -0300

    criação Repository e entidade User

---
+++ src/main/java/com/marleimelchior/projectSpringRestFull/ProjectSpringRestFullApplication.java
@@ -0,0 +1,13 @@
+package com.marleimelchior.projectSpringRestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringRestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marleimelchior/projectSpringRestFull/ProjectSpringRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marleimelchior.projectSpringRestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
